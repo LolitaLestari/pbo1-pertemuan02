81e3587d31f91812b94e0c0028801921c7da203b
LolitaLestari <lolita9a@gmail.com>
2020-03-25 14:56:31 +0800

    Memanggil method dari banyak class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ===============");
+  System.out.println("NPM : 18631022");
+  System.out.println("Nama : Lolita Lestari");
+  System.out.println("=============================");
+ }
+}
